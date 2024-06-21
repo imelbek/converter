e41dc2724adf708842035232fd88a4131375b5c1
imelbek <irakhimov98@gmail.com>
2024-06-21 22:00:10 +0500

    initial commit

---
+++ TempConverter/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter a value in Celsius: ");
+float c = float.Parse(Console.ReadLine());
+
+float f = c * 9 / 5 + 32;
+double k = c + 273.15;
+
+Console.WriteLine($"{c}°C equals to {f}°F");
+Console.WriteLine($"{c}°C equals to {k}K");
